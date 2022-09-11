526a86e23dd83327f76b86577c8c4390fc1906c4
yuherlim <110385648+yuherlim@users.noreply.github.com>
2022-09-11 16:02:34 +0800

    Create FileFunctions.java
    For classes that will have these basic functions to implement.

---
+++ ConvenienceStoreSystem/src/FileFunctions.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author Yu
+ */
+public interface FileFunctions {
+    void readFile(ArrayList<Object> obj, String filename);
+    void add();
+    void search();
+    void modify();
+    void display();
+}
