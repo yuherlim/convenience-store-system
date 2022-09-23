4a3c5cd18dcf75061c04946576537f3909efd868
Lim Jia Qing <jiaqing8999@gmail.com>
2022-09-23 17:13:23 +0800

    Daily report

--- ConvenienceStoreSystem/src/Report.java
+++ ConvenienceStoreSystem/src/Report.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 
 /**
@@ -5,23 +6,31 @@ import java.util.ArrayList;
  * @author JiaQing
  */
 public class Report {
+
     private String reportTitle;
     private String dateGenerated;
+    private String timeGenerated;
     private ArrayList<Transaction> transactions;
     private ArrayList<Product> products;
-    
+
     public Report() {
-        
+
     }
-    
+
     //Constructor
-    public Report(String reportTitle, String dateGenerated, ArrayList<Transaction> transactions, ArrayList<Product> products){
+    public Report(String reportTitle, String dateGenerated, ArrayList<Transaction> transactions, ArrayList<Product> products) {
         this.reportTitle = reportTitle;
         this.dateGenerated = dateGenerated;
         this.transactions = transactions;
         this.products = products;
     }
-    
+
+    public Report(String reportTitle, String dateGenerated, String timeGenerated) {
+        this.reportTitle = reportTitle;
+        this.dateGenerated = dateGenerated;
+        this.timeGenerated = timeGenerated;
+    }
+
     //getter and setter
     public String getReportTitle() {
         return reportTitle;
@@ -37,6 +46,14 @@ public class Report {
 
     public void setDateGenerated(String dateGenerated) {
         this.dateGenerated = dateGenerated;
+    }
+
+    public String getTimeGenerated() {
+        return timeGenerated;
+    }
+
+    public void setTimeGenerated(String timeGenerated) {
+        this.timeGenerated = timeGenerated;
     }
 
     public ArrayList<Transaction> getTransactions() {
@@ -55,23 +72,60 @@ public class Report {
         this.products = products;
     }
     
-    public static void dailyReport(){
-        
-    }
-    
-    public static void monthlyReport(){
-        
-    }
-    
-    //displays the reorder report for products that needs to be restocked.
-    public static void productReorderReport(){
+    public static void dailyReport(Staff staffLogin) {
         General.clearScreen();
+        staffLogin.setName("Lim Jia QIng");
         
+        Report dailyReport = new Report("Daily Report", General.getCurrentDateTime("date"), General.getCurrentDateTime("time"));
+        int transactionCount = 0;
+
+        //Prompt and get date
+        String checkDate = General.dateInput("Date of report to generate (DD/MM/YYYY): ", "  Invalid date format.");
+
+        //get all transaction into arraylist
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        TransactionDriver.readFile("src\\transaction.txt", transactions);
+
+        //print report header
+        System.out.println("==============");
+        System.out.println(' ' + dailyReport.getReportTitle());
+        System.out.println("==============");
+
+        System.out.println("Staff Generated: " + staffLogin.getName());
+        System.out.println("Date Generated: " + dailyReport.getDateGenerated() + "    Time Generated" + dailyReport.getTimeGenerated());
+        System.out.println('\n' + "Report Date: " + checkDate);
+        System.out.println("-----------------------------------------------------------------------------------------------------");
+        System.out.println("| Transaction ID | Member ID |  Payment ID  | Amount(RM) |                Staff Name                | ");
+        System.out.println("-----------------------------------------------------------------------------------------------------");
+        for (Transaction transaction : transactions) {
+            if (transaction.getDateTime().substring(0, 10).equals(checkDate)) {
+                System.out.printf("|  %12s  | %9s | %12s | %7.2f | %-40s |", transaction.getId(), transaction.getMember(), transaction.getPayment(), transaction.getPayment(), transaction.getStaff());
+                transactionCount++;
+            }
+        }
+        if (transactionCount == 0) {
+            System.out.println("| There are no transaction in " + checkDate + String.format("%59s", " ") + '|');
+        }
+        System.out.println("-----------------------------------------------------------------------------------------------------");
+        System.out.println("");
+        System.out.printf("< %d record(s) >\n", transactionCount);
+        System.out.println("");
+        General.systemPause();
+    }
+
+    public static void monthlyReport() {
+
+    }
+
+    //displays the reorder report for products that needs to be restocked.
+    public static void productReorderReport() {
+        General.clearScreen();
+
         //Create a new report obj
         Report reorderReport = new Report("Reorder Report", General.getCurrentDateTime("date"), null, Product.readFile(Product.FILE_NAME));
-       
+
         int printCount = 0;
-        
+
         //Printing report header
         System.out.println("==============");
         System.out.println(reorderReport.getReportTitle());
@@ -81,7 +135,7 @@ public class Report {
         System.out.println("--------------------------------------------------------------");
         System.out.println("| Code  | Name                 | Quantity | Reorder Quantity |");
         System.out.println("--------------------------------------------------------------");
-        for (Product p: reorderReport.products) {
+        for (Product p : reorderReport.products) {
             if (p.getStockQty() < p.getMinReorderQty()) {
                 if (p.getStatus().equals("ACTIVE")) {
                     System.out.printf("| %5s | %20s | %8d | %16d |\n", p.getCode(), p.getName(), p.getStockQty(), p.getMinReorderQty());
@@ -89,11 +143,10 @@ public class Report {
                 }
             }
         }
-        
+
         if (printCount == 0) {
             System.out.println("There are no products that needs to be reordered.");
         }
-        
 
         System.out.println("");
         System.out.printf("< %d record(s) >\n", printCount);
